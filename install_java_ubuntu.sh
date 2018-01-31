8476cb2842f8ade101997e86928de378fcdd3063
mausrdr <mausrdr@gmail.com>
2018-01-31 09:06:36 -0200

    Automate to get the URL of the desired package
    Automate to get the URL of the desired package. Added the functions get_package() and init_reset_javalink_files().

--- install_java_ubuntu.sh
+++ install_java_ubuntu.sh
@@ -3,17 +3,14 @@
 #################################################
 #                 Elaborado por:                #
 #         Mauro Augusto Soares Rodrigues        #
-#                      v3.2                     #
+#                      v4.0                     #
 #################################################
 #
 # Script para instalacao do java jre ou jdk
-# Para executar utilize o seguinte comando: ./install_java_ubuntu_server.sh num_vresao num_descompactado plataforma
-# Onde num_versao voce ira colocar o numero da versao corrente do java
-# num_descompactado sera iniciado pelo numeral 1 seguido de um ponto ".", mais o primeiro numero da versao,
-# seguido de outro ponto ".", o numeral zero, o caractere underline "_" e os dois ultimos numeros da
-# versao. Ex.: se a versao for 7u67 o num_descompactado sera 1.7.0_67
+# Para executar utilize o seguinte comando: ./install_java_ubuntu_server.sh num_vresao plataforma
+# num_versao e a versao corrente do java.
 # E plataforma indicara se sera instalado jre ou jdk
-# Ex.: ./install_java_ubuntu_server.sh 7u67 1.7.0_67 jre
+# Ex.: ./install_java_ubuntu_server.sh 9.0.4 jre
 
 function init_reset_db_files() {
 	if [[ -e /root/older_java_jre.db ]]; then
@@ -21,6 +18,15 @@ function init_reset_db_files() {
 	fi
 	if [[ -e /root/older_java_jdk.db ]]; then
 		rm -rfv /root/older_java_jdk.db;
+	fi
+}
+
+function init_reset_javalink_files() {
+	if [[ -e /root/javalink ]]; then
+		rm -rfv /root/javalink;
+	fi
+	if [[ -e /root/javalinkdown ]]; then
+		rm -rfv /root/javalinkdown;
 	fi
 }
 
@@ -38,12 +44,12 @@ function remove_previous() {
 	if [[ $amount_jdk -ne 0 || $amount_jre -ne 0 ]]; then
 		if [[ $amount_jdk -eq 0 ]]; then
 			if [[ $amount_jre -eq 1 ]]; then
-				older_java=$(ls /usr/java | grep jre | cut -d '' -f 1)
+				older_java=$(ls /usr/java | grep jre | cut -d '' -f1)
 				update-alternatives --remove java /usr/java/$older_java/bin/java;
 				update-alternatives --remove javaws /usr/java/$older_java/bin/javaws;
 				rm -rfv /usr/java/$older_java;
 			else
-				older_java=$(ls /usr/java | grep jre | cut -d '' -f 1)
+				older_java=$(ls /usr/java | grep jre | cut -d '' -f1)
 				echo $older_java > older_java_jre.db;
 				while read entry_older_jre; do
 					update-alternatives --remove java /usr/java/$entry_older_jre/bin/java;
@@ -53,12 +59,12 @@ function remove_previous() {
 			fi
 		else
 			if [[ $amount_jdk -eq 1 ]]; then
-				older_java=$(ls /usr/java | grep jdk | cut -d '' -f 1)
+				older_java=$(ls /usr/java | grep jdk | cut -d '' -f1)
 				update-alternatives --remove java /usr/java/$older_java/jre/bin/java;
 				update-alternatives --remove javaws /usr/java/$older_java/jre/bin/javaws;
 				rm -rfv /usr/java/$older_java;
 			else
-				older_java=$(ls /usr/java | grep jdk | cut -d '' -f 1)
+				older_java=$(ls /usr/java | grep jdk | cut -d '' -f1)
 				echo $older_java > older_java_jdk.db;
 				while read entry_older_jdk; do
 					update-alternatives --remove java /usr/java/$entry_older_jdk/jre/bin/java;
@@ -67,6 +73,35 @@ function remove_previous() {
 				done < /root/older_java_jdk.db
 			fi
 		fi
+	fi
+}
+
+function get_package() {
+	curl -fLC - --retry 3 --retry-delay 3 -b oraclelicense=a -o javalink http://www.oracle.com/technetwork/java/javase/downloads/index.html
+	if [[ $1 == jdk ]]; then
+		jdk_str="jdk9-downloads"
+		for (( i = 1; i < 1000; i++ )); do
+			link_url=`grep  ">JRE" javalink | tr -s " " | tr -d \" | sed '/>JRE/!d' | cut -d= -f$i | cut -d" " -f1`
+			if [[ ${link_url,,} =~ $jdk_str ]]; then
+				curl -fLC - --retry 3 --retry-delay 3 -b oraclelicense=a -o javalinkdown $link_url
+				down_url=`grep "\['$1-$2_linux-x64_bin.tar.gz'\]" javalinkdown | tr -d \" | cut -d: -f4,5 | cut -d, -f1`
+				wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $down_url
+				break
+			fi
+		done
+	else
+		jre_str="jre9-downloads"
+		for (( i = 0; i < 10; i++ )); do
+			#statements
+			link_url=`grep  ">JRE" javalink | tr -s " " | tr -d \" | sed '/>JRE/!d' | cut -d= -f$i | cut -d" " -f1`
+			if [[ ${link_url,,} =~ $jre_str ]]; then
+				#statements
+				curl -fLC - --retry 3 --retry-delay 3 -b oraclelicense=a -o javalinkdown $link_url
+				down_url=`grep "\['$1-$2_linux-x64_bin.tar.gz'\]" javalinkdown | tr -d \" | cut -d: -f4,5 | cut -d, -f1`
+				wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $down_url
+				break
+			fi
+		done
 	fi
 }
 
@@ -117,20 +152,21 @@ function set_env_variables() {
 
 init_reset_profile
 init_reset_db_files
+init_reset_javalink_files
 
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/$1-b11/d54c1d3a095b4ff2b6607d096fa80163/$3-$1-linux-x64.tar.gz;
-tar -zxvf $3-$1-linux-x64.tar.gz;
-rm -rfv $3-$1-linux-x64.tar.gz;
+get_package $2 $1
+tar -zxvf $2-$1_linux-x64_bin.tar.gz;
+rm -rfv $2-$1_linux-x64_bin.tar.gz;
 
 
 if [[ -d /usr/java ]]; then
-	install_java $3 $2 1
+	install_java $2 $1 1
 else
   mkdir /usr/java;
-  install_java $3 $2
+  install_java $2 $1
 fi
 
-set_env_variables $3 $2
+set_env_variables $2 $1
 . /etc/profile;
 clear;
 java -version;
